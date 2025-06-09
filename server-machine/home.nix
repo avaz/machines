0671cb5bb1938339529b564ed6dd1eb7723011db
Anderson Vaz <dev@andersonvaz.com>
2025-06-09 15:26:43 +0200

    Adds basic server-machine configuration.

---
+++ server-machine/home.nix
@@ -0,0 +1,30 @@
+{ config, pkgs, username, ... }:
+
+{
+  users.users.${username} = {
+    name = "${username}";
+    home = "/Users/${username}";
+    isHidden = false;
+    shell = pkgs.zsh;
+  };
+  home-manager = {
+    useGlobalPkgs = true;
+    users.${username}= { pkgs, config, lib, ... }:{
+       home = {
+          packages = with pkgs; [
+            htop
+            git-town
+            gh
+            docker-credential-helpers
+            oh-my-zsh
+          ];
+          sessionVariables = {
+            NIX_SHELL_PYTHON = "nix develop github:avaz/machines#python";
+            NIX_SHELL_JAVA = "nix develop github:avaz/machines#java";
+          };
+          stateVersion = "23.11";
+      };
+    };
+  };
+
+}
