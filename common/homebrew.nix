0a1fba592ccd16431eba262838e2b71740c6009f
Anderson Vaz <dev@andersonvaz.com>
2026-03-11 20:59:03 +0100

    Adds mas and other missing apps to brew configuration.

--- common/homebrew.nix
+++ common/homebrew.nix
@@ -14,6 +14,7 @@
     ];
 
     brews = [
+        "mas"
     ];
 
     casks = [
--- main-machine/homebrew.nix
+++ main-machine/homebrew.nix
@@ -13,9 +13,13 @@
       "google-chrome"
       "shottr"
       "ghostty"
+      "basecamp"
+      "keymapp"
     ];
 
     masApps = {
+      "Agenda" = 1287445660;
+      "Motion Graphics Linearity Move" = 6443677011;
       "Curve" = 1219074514;
       "Developer" = 640199958;
       "Linearity Move" = 6443677011;
@@ -24,6 +28,9 @@
       "Pixelmator Pro" = 1289583905;
       "Reeder" = 1529448980;
       "Xcode" = 497799835;
+      "Craft" = 1487937127;
+      "Mactracker" = 430255202;
+      "Magnet" = 441258766;
     };
   };
 }
