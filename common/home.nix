ae7da0071112e3a278df9ffa48a4c7cdfb2d9b18
Anderson Vaz <dev@andersonvaz.com>
2026-04-19 17:26:07 +0200

    Adds common packages to `common/home.nix`.

--- common/home.nix
+++ common/home.nix
@@ -3,6 +3,13 @@
 {
   home.username = username;
   home.stateVersion = "23.11";
+  home.packages = with pkgs; [
+    # Common packages for all machines
+    tor
+    direnv
+    gh
+    git-town
+  ];
 
   # Add other common programs/settings here
   # These will be inherited by all machines unless overridden
